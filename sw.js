c4d40d6f8f9a29be9063cfbe46971497e03c8341
desina-hq <desina.com@gmail.com>
2026-04-28 16:21:14 +0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('desina-store').then((cache) => cache.addAll([
+      '/',
+      '/index-ar.html',
+      '/logo-512.png'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
